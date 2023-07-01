8b062cacd77af912da15aa70c77efd3634dae8e9
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-07-01 09:39:41 +0000

    added task 7

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -3,8 +3,9 @@ export default class Airport {
     this._name = name;
     this._code = code;
   }
+
   [Symbol.for('nodejs.util.inspect.custom')]() {
-    return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`; // eslint-disable-line
+    return `Airport [${this._code}] { _name: ${this._name}, _code: ${this._code} }`; // eslint-disable-line
   }
 
   toString() {
