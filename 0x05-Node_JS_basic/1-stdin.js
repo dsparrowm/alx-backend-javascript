6f274e3dd7e90b0275c24b53acd0e3c618f5d1b7
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-08-21 21:15:09 +0000

    added task 0 and 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+if (process.stdin.isTTY) {
+	process.stdin.on('data', (data) => {
+		process.stdout.write(`Your name is: ${data}`);
+		process.exit();
+	});
+} else {
+	process.on('data', (data) => {
+		process.stdout.write(`Your name is: ${data}`);
+		process.exit();
+	});
+	process.on('exit', () => {
+		process.stdout.write('This important software is now closing\n');
+	});
+}
